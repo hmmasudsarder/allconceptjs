a375d89158eb24eb7339c5ef15abe1cd5ffad2f8
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-09-09 00:01:45 +0600

    this keywork

---
+++ this-keyword.js
@@ -0,0 +1,13 @@
+function greet(greeting) {
+    console.log( this.name+ ', ' + greeting +', '+ this.job);
+  }
+  
+  const person = { name: 'cr7', job: "footboll" };
+ const team =["ayan", "monir", "madiha"];
+ const leader = {
+    name: "israt",
+    job: "web Developer"
+ }
+  
+  greet.apply(person, leader, team);  // Output: Hello, Alice!
+  
