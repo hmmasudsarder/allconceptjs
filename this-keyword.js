c2b49e359a26dcc0136a7699afb600bb5f1a596b
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-09-12 10:53:48 +0600

    some issue solve

--- this-keyword.js
+++ this-keyword.js
@@ -1,14 +1,12 @@
-function greet(greeting) {
-    console.log( this.name+ ', ' + greeting +', '+ this.job);
-  }
-  
-  const person = { name: 'cr7', job: "footboll" };
- const team =["ayan", "monir", "madiha"];
- const leader = {
-    name: "israt",
-    job: "web Developer"
- }
-  
-  greet.apply(person, leader, team);  // Output: Hello, Alice!
-  
-  const hello = "Hello"
+// call(), bind(), apply(), function 
+// Proble statement 
+
+const userDtails = {
+   name: "masud",
+   age: 23,
+   Designation: "web Developer",
+   printSomeIssue: function () {
+      
+   }
+}
+console.log(userDtails.name);
