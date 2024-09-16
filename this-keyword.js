0b117a00bd0999ddc4df9b7cc1ed436627675494
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-09-16 06:23:50 +0600

    call function added

--- this-keyword.js
+++ this-keyword.js
@@ -1,12 +1,21 @@
 // call(), bind(), apply(), function 
 // Proble statement 
 
+// const userDtails = {
+//    name: "masud",
+//    age: 23,
+//    Designation: "web Developer",
+//    printSomeIssue: function () {
+      
+//    }
+// }
+// console.log(userDtails.name);
+function great() {
+      console.log(`Mr ${this.name} is a ${this.Designation}`)
+}
 const userDtails = {
    name: "masud",
    age: 23,
    Designation: "web Developer",
-   printSomeIssue: function () {
-      
-   }
 }
-console.log(userDtails.name);
+great.call(userDtails)
