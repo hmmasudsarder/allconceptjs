b5692c707c064ae8d89ad9147079c77a2bcfa6cd
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-09-18 06:49:22 +0600

    callbackfuction problem solve

---
+++ callback.js
@@ -0,0 +1,29 @@
+// function myDisplayer(something) {
+//     document.getElementById("demo").innerHTML = something;
+//   }
+  
+//   function myCalculator(num1, num2, myCallback) {
+//     let sum = num1 + num2;
+//     myCallback(sum);
+//   }
+  
+//   myCalculator(5, 5, myDisplayer);
+
+  const myNumbers = [4, 1, -20, -7, 5, 9, -6];
+
+// Call removeNeg with a Callback
+const posNumbers = removeNeg(myNumbers, (x) => x >= 0);
+
+// Display Result
+document.getElementById("demo").innerHTML = posNumbers;
+
+// Remove negative numbers
+function removeNeg(numbers, callback) {
+  const myArray = [];
+  for (const x of numbers) {
+    if (callback(x)) {
+      myArray.push(x);
+    }
+  }
+  return myArray;
+}
