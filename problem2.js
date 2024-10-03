ace2526287f521e6249d44ad09844e2414788774
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-10-03 20:54:15 +0600

    added equel

--- problem2.js
+++ problem2.js
@@ -1,11 +1,29 @@
-const arrayNumber =[9];
+// const arrayNumber =[9];
 
-const array1 = [2, 4, 5, 56, 1, 3, 9];
-let sum = 0;
-for (const element of array1) {
-    if (element === 9) {
-        sum ++
-    }
+// const array1 = [2, 4, 5, 56, 1, 3, 9];
+// let sum = 0;
+// for (const element of array1) {
+//     if (element === 9) {
+//         sum ++
+//     }
+// }
+
+// console.log(sum);
+
+// bigger >
+// less <
+
+// for (let i = 100; i > 0; i--) {
+//     console.log(i);    
+// }
+
+const forLoopPare = true;
+let mathAll = 13;
+let timeMentine = 13;
+
+
+if (mathAll > 12 && timeMentine > 12 && forLoopPare == true) {
+    console.log("your selected");
+}else{
+    console.log("try next time");
 }
-
-console.log(sum);
