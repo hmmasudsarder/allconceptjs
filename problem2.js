198761af7c3afdde01f3cb7ae864fe70801f64d5
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-10-03 10:18:05 +0600

    solve two proble

---
+++ problem2.js
@@ -0,0 +1,11 @@
+const arrayNumber =[9];
+
+const array1 = [2, 4, 5, 56, 1, 3, 9];
+let sum = 0;
+for (const element of array1) {
+    if (element === 9) {
+        sum ++
+    }
+}
+
+console.log(sum);
