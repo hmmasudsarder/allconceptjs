cf1034f4e1960149f6e53de5a0051830a690c794
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-10-18 18:18:24 +0600

    compaier subString and Slice

--- string.js
+++ string.js
@@ -1,4 +1,7 @@
 let name = "hello Dhaka"
 
 let text = "Hello world!";
-let result = text.substring(1, 4);
+let result = text.substring(0, 4);
+let result1 = text.slice(0, 5)
+console.log(result1);
+console.log(result);
