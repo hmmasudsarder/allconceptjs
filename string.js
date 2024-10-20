01f1ff97f433a359fb1f23f4d36561eff35b38ed
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-10-20 10:55:51 +0600

    test padStart function in javascript

--- string.js
+++ string.js
@@ -1,7 +1,11 @@
-let name = "hello Dhaka"
+// let name = "hello Dhaka"
 
-let text = "Hello world!";
-let result = text.substring(0, 4);
-let result1 = text.slice(0, 5)
-console.log(result1);
+// let text = "Hello world!";
+// let result = text.substring(0, 4);
+// let result1 = text.slice(0, 5)
+// console.log(result1);
+// console.log(result);
+
+let orderId = 120;
+const result = orderId.toString().padStart(5, 0)
 console.log(result);
